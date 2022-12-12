80a6605bf1850df8c5440bc22ccb336aee7bc87b
robledo66 <111396707+robledo66@users.noreply.github.com>
2022-12-11 21:58:04 -0300

    Add files via upload
    Tp1 de programación.
    Robledo Joel  dwncv 2

---
+++ array.js
@@ -0,0 +1,38 @@
+// array de discos//
+
+
+/*
+* ROBLEDO,JOEL | ROBLEDO, JOEL
+*/
+
+Megadeto = {
+ Nombre: 'Megadeto',
+ Autor: 'Los Programadores Anónimos',
+ Codigo: 1,
+ Pistas: [
+     {
+         Nombre: 'Esa cajita loca llamada variablecita',
+ Duración: 200,
+ },
+ {
+ Nombre: 'Nunca quise ser un NaN',
+ Duración: 180,
+ },
+ {
+ Nombre: 'No quiero programar',
+ Duración: 90,
+ },
+ {
+ Nombre: 'Bajo presion',
+  Duración: 240,
+ },
+ {
+ Nombre: 'La odisea de las variables privadas',
+ Duración: 120,
+ },
+ {
+   Nombre: 'Sr. Programador',
+ Duración: 720,
+ },
+ ],
+ };
